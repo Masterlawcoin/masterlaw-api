2668182f418f1ac8ba81542547188a4b0ff6a8f8
Masterlaw <87247129+Masterlawcoin@users.noreply.github.com>
2026-08-26 04:54:12 -0400

    Refresh VCH PWA cache for admin and worker roles

--- vch-control-jornada/service-worker.js
+++ vch-control-jornada/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='vch-jornada-v2';
+const CACHE='vch-jornada-v5';
 const FILES=['./','./index.html','./config.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
