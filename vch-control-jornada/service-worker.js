4928d7ef583f96ec009c6d94c1bdfd381f36b3c4
Masterlaw <87247129+Masterlawcoin@users.noreply.github.com>
2026-08-26 01:05:06 -0400

    Add VCH offline shell

---
+++ vch-control-jornada/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='vch-jornada-v1';
+const FILES=['./','./index.html','./config.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
