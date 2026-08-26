6eb5fa550ab3ccd42c024de70211cca117b34e4e
Masterlaw <87247129+Masterlawcoin@users.noreply.github.com>
2026-08-26 03:34:37 -0400

    Refresh VCH File Manager cache

--- vch-control-jornada/service-worker.js
+++ vch-control-jornada/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='vch-jornada-v1';
+const CACHE='vch-jornada-v2';
 const FILES=['./','./index.html','./config.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
