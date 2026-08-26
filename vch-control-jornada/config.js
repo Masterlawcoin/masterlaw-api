4dff8a91574e0af10266d32753e68abb345bd38f
Masterlaw <87247129+Masterlawcoin@users.noreply.github.com>
2026-08-26 04:53:55 -0400

    Add VCH admin user management function config

--- vch-control-jornada/config.js
+++ vch-control-jornada/config.js
@@ -2,5 +2,6 @@ window.VCH_CONFIG = {
   SUPABASE_URL: 'https://tjyplcnvwjzpfcbctlrs.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_1KBU2xqzuzPCKr3Vfl4xjA_h7xtlh-a',
   REPORT_FUNCTION: 'vch-gerencia-report',
+  ADMIN_FUNCTION: 'vch-admin-users',
   TIMEZONE: 'America/Santiago'
 };
